425dc09391168937b0cd466a249806d28398f7e6
MachineMitch21 <mitchschutt@gmail.com>
2017-10-11 11:39:10 -0400

    changes to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,19 @@
-// #include <AntisocialDLL.h>
-// #include <Windows.h>
-// #include <Window.h>
+#include <AntisocialDLL.h>
+#include <Windows.h>
+#include <Window.h>
 #include <fstream>
 
-//using antisocial::Window;
+using antisocial::Window;
 
 int main(int argv, char** argc)
 {
-    std::ofstream logFile("antisocialLog.txt");
+    Window w("Hello, Antisocial", 800, 600);
 
-    if (logFile.is_open())
+    while(!w.IsClosed())
     {
-        logFile << "Hello, world\n";
-    }
+        w.clear(0.1f, 0.1f, 0.1f, 1.0f);
 
-    logFile.close();
-    // HINSTANCE hinstDLL;
-    //
-    // hinstDLL = LoadLibrary("libantisocial.dll");
-    //
-    // if (hinstDLL != NULL)
-    // {
-    //     Window w("Hello, Antisocial", 800, 600);
-    //
-    //     while(!w.IsClosed())
-    //     {
-    //         w.clear(0.1f, 0.1f, 0.1f, 1.0f);
-    //
-    //         w.update();
-    //     }
-    // }
-    // else
-    // {
-    //     std::ofstream logFile("antisocialLog.txt");
-    //
-    //     if (logFile.is_open())
-    //     {
-    //         logFile << "Did not load dll\n";
-    //     }
-    //
-    //     logFile.close();
-    // }
+        w.update();
+    }
     return 0;
 }
