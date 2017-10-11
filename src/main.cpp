d19d7e1963e72ee294e5660e2523597891dbc7ea
MachineMitch21 <mitchschutt@gmail.com>
2017-10-11 11:25:22 -0400

    Testing just main with a file

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,45 @@
-#include <AntisocialDLL.h>
-#include <Windows.h>
-#include <Window.h>
+// #include <AntisocialDLL.h>
+// #include <Windows.h>
+// #include <Window.h>
 #include <fstream>
 
-using antisocial::Window;
+//using antisocial::Window;
 
 int main(int argv, char** argc)
 {
-    HINSTANCE hinstDLL;
+    std::ofstream logFile("antisocialLog.txt");
 
-    hinstDLL = LoadLibrary("libantisocial.dll");
-
-    if (hinstDLL != NULL)
+    if (logFile.is_open())
     {
-        Window w("Hello, Antisocial", 800, 600);
-
-        while(!w.IsClosed())
-        {
-            w.clear(0.1f, 0.1f, 0.1f, 1.0f);
-
-            w.update();
-        }
+        logFile << "Hello, world\n";
     }
-    else
-    {
-        std::ofstream logFile("antisocialLog.txt");
 
-        if (logFile.is_open())
-        {
-            logFile << "Did not load dll\n";
-        }
-
-        logFile.close();
-    }
+    logFile.close();
+    // HINSTANCE hinstDLL;
+    //
+    // hinstDLL = LoadLibrary("libantisocial.dll");
+    //
+    // if (hinstDLL != NULL)
+    // {
+    //     Window w("Hello, Antisocial", 800, 600);
+    //
+    //     while(!w.IsClosed())
+    //     {
+    //         w.clear(0.1f, 0.1f, 0.1f, 1.0f);
+    //
+    //         w.update();
+    //     }
+    // }
+    // else
+    // {
+    //     std::ofstream logFile("antisocialLog.txt");
+    //
+    //     if (logFile.is_open())
+    //     {
+    //         logFile << "Did not load dll\n";
+    //     }
+    //
+    //     logFile.close();
+    // }
     return 0;
 }
