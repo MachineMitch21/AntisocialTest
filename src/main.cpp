590e42cdfd88d1be84d6468027e55d874aea6555
MachineMitch21 <mitchschutt@gmail.com>
2017-10-11 14:35:36 -0400

    Added new Input.h header file and tested left mouse click

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <AntisocialDLL.h>
 #include <Windows.h>
 #include <Window.h>
+#include <Input.h>
 #include <fstream>
+#include <iostream>
 
 using antisocial::Window;
+using namespace antisocial::input;
 
 int main(int argv, char** argc)
 {
     Window w("Hello, Antisocial", 800, 600);
+    Input i;
 
     while(!w.IsClosed())
     {
+        if (Input::mouseButtonDown(MouseButton::M_LEFT))
+        {
+            std::cout << "Clicked left mouse button" << std::endl;
+        }
+
         w.clear(0.1f, 0.1f, 0.1f, 1.0f);
 
         w.update();
