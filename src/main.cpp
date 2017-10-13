cd3a9dfe0741e7a697591b88e16fca8298c87c97
MachineMitch21 <mitchschutt@gmail.com>
2017-10-13 00:13:42 -0400

    Trying to draw triangle

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include <GL/glew.h>
+#include <vector>
 
 using antisocial::Window;
 using namespace antisocial::input;
@@ -12,8 +13,27 @@ using namespace antisocial::input;
 int main(int argv, char** argc)
 {
     Window w("Hello, Antisocial", 800, 600);
-
     Input::updateContext(w.getContext());
+
+    std::vector<float> vertices =
+    {
+        -0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f
+    };
+
+    GLuint vao, vbo;
+
+    glGenBuffers(1, &vbo);
+    glGenVertexArrays(1, &vao);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(0));
 
     while(!w.IsClosed())
     {
@@ -29,7 +49,7 @@ int main(int argv, char** argc)
 
         w.clear(0.1f, 0.1f, 0.1f, 1.0f);
 
-        //** RENDER!! **//
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         w.update();
     }
